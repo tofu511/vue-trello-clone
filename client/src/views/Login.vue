cd7eee04358632fce904f51b3b0cdfed6df25a14
tofu511 <mokmok511@gmail.com>
2020-02-27 17:16:52 +0900

    Feat: login function

---
+++ client/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <v-form
+          v-if="!loading"
+          v-model="valid"
+          @submit.prevent="login"
+          @keydown.prevent.enter
+          >
+          <v-text-field
+            v-model="user.username"
+            :rules="notEmptyRules"
+            label="Username"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.password"
+            :rules="notEmptyRules"
+            label="Password"
+            type="password"
+            required
+          ></v-text-field>
+          <v-btn type="submit" :disabled="!valid">Login</v-btn>
+        </v-form>
+        <v-progress-circular
+          v-if="loading"
+          indeterminate
+          color="primary">
+        </v-progress-circular>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'login',
+  data: (vm) => ({
+    valid: false,
+    user: {
+      username: '',
+      password: ''
+    },
+    notEmptyRules: [(value) => !!value || 'Cannot be empty.']
+  }),
+  computed: {
+    ...mapState('auth', { loading: 'isAuthenticatePending' })
+  },
+  methods: {
+    ...mapActions('auth', ['authenticate']),
+    login () {
+      if (this.valid) {
+        this.authenticate({
+          strategy: 'local',
+          ...this.user
+        }).then(() => {
+          console.log('logged in!')
+        }).catch(e => {
+          console.error('Authentication error', e)
+        })
+      }
+    }
+  }
+}
+</script>
