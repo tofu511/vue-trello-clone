0b491a29de9ac8a9248128f9ca31e3e51a57c2dc
tofu511 <mokmok511@gmail.com>
2020-03-10 22:24:03 +0900

    Feat: add restriction to card hook

--- server/src/services/cards/cards.hooks.js
+++ server/src/services/cards/cards.hooks.js
@@ -1,8 +1,9 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const isBoardOwner = require('../../hooks/autholization');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [ authenticate('jwt'), isBoardOwner() ],
     find: [],
     get: [],
     create: [],
